dd0c33e220fe1b3711c48207f6a8fb7533249adf
Carlos Hernandez <charlygago96@gmail.com>
2026-03-09 10:31:24 +0100

    preparando el entorno

---
+++ src/test/java/com/banking/banking_transfers/BankingTransfersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.banking.banking_transfers;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BankingTransfersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
